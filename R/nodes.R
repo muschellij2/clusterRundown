66800dfbcdbd33bf3e5aa6160c2dbbfc086c4377
muschellij2 <muschellij2@gmail.com>
2015-05-05 21:09:22 -0400

    Done

--- R/nodes.R
+++ R/nodes.R
@@ -51,34 +51,63 @@ full.rundown = function(username=NULL, all.q = TRUE, std.name = "shared"){
   
   df = df[ grepl("=", df$x), ]
   df$resource = gsub("(.*)=(.*)", "\\1", df$x)
-  df$value = gsub("(.*)=(.*)", "\\2", df$x)
+  df$value = gsub("(.*)=(.*)", "\\2", toupper(df$x))
   df$q = gsub("(.*)[.]q", "\\1", df$queue)
   df = df[ df$resource != df$q, ]
   df$q = NULL
   df$x = NULL
   df$obj = NULL
+  df$value[grepl("K$", df$value)] = gsub("K$", "", 
+                                         df$value[grepl("M$", df$value)])
+  df$value[grepl("M$", df$value)] = gsub("M$", "000", 
+                                      df$value[grepl("M$", df$value)])
+  df$value[grepl("G$", df$value)] = gsub("G$", "000000", 
+                                      df$value[grepl("G$", df$value)])
+  
+  long = df
+
   df = reshape(df, direction = "wide", 
                idvar = "id",
                timevar = "resource", 
                times = unique(df$resource),
                v.names = "value")
   colnames(df) = gsub("value[.]", "", colnames(df))
-  
   df = merge(all.ids, df, all.x = TRUE, sort = FALSE)
   df = df[ order(df$id), ]
   df$cores = as.numeric(df$cores)
+  cn = colnames(df)
+  func = function(df, cname){
+    if (!(cname %in% cn)){
+      df[, cname] = NA
+    }
+    return(df)
+  }
+  for (cname in c("h_fsize", 
+                  "h_stack", "h_vmem", "mem_free")){
+    df = func(df, cname)
+  }
+  xdf = df
+  #### turn into Gb
+  df$mem_free = as.numeric(df$mem_free) / (1000 * 1000)
+  df$h_stack = as.numeric(df$h_stack) / (1000 * 1000)
+  df$h_fsize = as.numeric(df$h_fsize) / (1000 * 1000)
+  df$h_vmem = as.numeric(df$h_vmem) / (1000 * 1000)
   
-  xx = ddply(df, .(queue), function(d){
-    ddply(d, .(user), function(x){
-      c(jobs = length(x$cores), cores = sum(x$cores))
-    })
+  user = ddply(df, .(user), function(d){
+    colSums()
   })
-  xx = xx[ xx$jobs > 0,]
-  xx = xx[order(xx$queue, xx$cores, xx$user), ]
-
-  ncores = ddply(df, .(user), function(x){
-    c(jobs = length(x$cores), cores = sum(x$cores))
-  })
+  
+#   xx = ddply(df, .(queue), function(d){
+#     ddply(d, .(user), function(x){
+#       c(jobs = length(x$cores), cores = sum(x$cores))
+#     })
+#   })
+#   xx = xx[ xx$jobs > 0,]
+#   xx = xx[order(xx$queue, xx$cores, xx$user), ]
+# 
+#   ncores = ddply(df, .(user), function(x){
+#     c(jobs = length(x$cores), cores = sum(x$cores))
+#   })
   
   return(df)
 }
