f944724f0c381fd30549c5f9c022c6785cd20b56
muschellij2 <muschellij2@gmail.com>
2021-03-24 14:23:39 -0400

    updated jobs-table

--- R/jobs_table.R
+++ R/jobs_table.R
@@ -3,19 +3,21 @@
 #' @param sort sort the data
 #' @export 
 jobs_table = function(sort = TRUE) {
-  n = job = node = NULL
-  rm(list = c("job", "node", "n"))
+  status = n = job = node = NULL
+  rm(list = c("job", "node", "n", "status"))
   out = system('qstat', intern = TRUE)
   out = out[3:length(out)]
   out = gsub(" +", " ", out)
   out = trimws(out)
   df = strsplit(out, split = " ")
   
-  df = lapply(df, getslot, c(3, 8))
+  df = lapply(df, getslot, c(3, 5, 8))
   df = do.call(rbind, df)
-  colnames(df) = c("job", "node")
+  colnames(df) = c("job", "status", "node")
   df = as.data.frame(df, stringsAsFactors = FALSE)
   df$node = sub(".*compute-(\\d*)[.].*", "\\1", df$node)
+  df = df %>% 
+    dplyr::filter(!status %in% c("qw", "Eqw"))
   df = df %>% 
     dplyr::count(job, node)
   if (sort) {
